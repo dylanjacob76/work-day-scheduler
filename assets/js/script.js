460e2cd223e6072136b95c9a5827cb54466a17f4
dylanjacob76 <business.dmjacob@gmail.com>
2022-11-22 10:02:26 -0500

    finished rough draft of final application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,14 @@
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
 var currentDayEl = $("#currentDay");
 var containerEl = $(".container");
-
-// Displays the current day at the top of the calender
+var saveBtnEl = $(".saveBtn");
 var currentDay = moment().format("dddd, MMMM Do");
 var pEl = $("<p>");
+var currentTime = moment().format("k");
+
+
+// Displays the current day at the top of the calender
 pEl.append(currentDay)
 currentDayEl.append(pEl);
-
-// Creates current time variable
-var currentTime = moment().format("k");
-console.log(currentTime);
 
 // Adds dynamic color class to to the text area based on the current hour
 // TODO: if the past, clear local storage for that specific hour
@@ -27,6 +17,7 @@ if (currentTime < 9) {
     hour9El.addClass("future");
 } else if (currentTime > 9) {
     hour9El.addClass("past");
+    localStorage.setItem("hour-9", "");
 } else {
     hour9El.addClass("present");
 }
@@ -36,6 +27,7 @@ if (currentTime < 10) {
     hour10El.addClass("future");
 } else if (currentTime > 10) {
     hour10El.addClass("past");
+    localStorage.setItem("hour-10", "");
 } else {
     hour10El.addClass("present");
 }
@@ -45,6 +37,7 @@ if (currentTime < 11) {
     hour11El.addClass("future");
 } else if (currentTime > 11) {
     hour11El.addClass("past");
+    localStorage.setItem("hour-11", "");
 } else {
     hour11El.addClass("present");
 }
@@ -54,6 +47,7 @@ if (currentTime < 12) {
     hour12El.addClass("future");
 } else if (currentTime > 12) {
     hour12El.addClass("past");
+    localStorage.setItem("hour-12", "");
 } else {
     hour12El.addClass("present");
 }
@@ -63,6 +57,7 @@ if (currentTime < 13) {
     hour13El.addClass("future");
 } else if (currentTime > 13) {
     hour13El.addClass("past");
+    localStorage.setItem("hour-13", "");
 } else {
     hour13El.addClass("present");
 }
@@ -72,6 +67,7 @@ if (currentTime < 14) {
     hour14El.addClass("future");
 } else if (currentTime > 14) {
     hour14El.addClass("past");
+    localStorage.setItem("hour-14", "");
 } else {
     hour14El.addClass("present");
 }
@@ -81,6 +77,7 @@ if (currentTime < 15) {
     hour15El.addClass("future");
 } else if (currentTime > 15) {
     hour15El.addClass("past");
+    localStorage.setItem("hour-15", "");
 } else {
     hour15El.addClass("present");
 }
@@ -90,6 +87,7 @@ if (currentTime < 16) {
     hour16El.addClass("future");
 } else if (currentTime > 16) {
     hour16El.addClass("past");
+    localStorage.setItem("hour-16", "");
 } else {
     hour16El.addClass("present");
 }
@@ -99,9 +97,38 @@ if (currentTime < 17) {
     hour17El.addClass("future");
 } else if (currentTime > 17) {
     hour17El.addClass("past");
+    localStorage.setItem("hour-17", "");
 } else {
     hour17El.addClass("present");
 }
 
-// on click of the save button save to local storage
 
+function saveSchedule() {
+    // Takes the value of the textarea and save it to local storage
+    localStorage.setItem("hour-9", hour9El.val());
+    localStorage.setItem("hour-10", hour10El.val());
+    localStorage.setItem("hour-11", hour11El.val());
+    localStorage.setItem("hour-12", hour12El.val());
+    localStorage.setItem("hour-13", hour13El.val());
+    localStorage.setItem("hour-14", hour14El.val());
+    localStorage.setItem("hour-15", hour15El.val());
+    localStorage.setItem("hour-16", hour16El.val());
+    localStorage.setItem("hour-17", hour17El.val());
+}
+
+function renderSchedule() {
+    // Takes the local storage value and sets the text area of the corresponding hour to its value
+    hour9El.text(localStorage.getItem("hour-9"));
+    hour10El.text(localStorage.getItem("hour-10"));
+    hour11El.text(localStorage.getItem("hour-11"));
+    hour12El.text(localStorage.getItem("hour-12"));
+    hour13El.text(localStorage.getItem("hour-13"));
+    hour14El.text(localStorage.getItem("hour-14"));
+    hour15El.text(localStorage.getItem("hour-15"));
+    hour16El.text(localStorage.getItem("hour-16"));
+    hour17El.text(localStorage.getItem("hour-17"));
+}
+
+renderSchedule();
+
+saveBtnEl.on("click", saveSchedule);
